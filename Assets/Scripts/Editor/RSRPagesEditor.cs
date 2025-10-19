d4c9cef44fda372c9486f85b9c2d3ba8adf5cf70
Maged Farid <magedf.90@gmail.com>
2025-10-20 00:06:42 +0300

    Separated all Cards logic from RSRPages

--- Assets/Scripts/Editor/RSRPagesEditor.cs
+++ Assets/Scripts/Editor/RSRPagesEditor.cs
@@ -6,31 +6,17 @@ namespace RecyclableSR
     public class RSRPagesEditor : RSREditor
     {
         private SerializedProperty _swipeThreshold;
-        private SerializedProperty _cardMode;
-        private SerializedProperty _cardZMultiplier;
-        private SerializedProperty _manuallyHandleCardAnimations;
 
         protected override void OnEnable()
         {
             base.OnEnable();
             _swipeThreshold = serializedObject.FindProperty(nameof(_swipeThreshold));
-            _cardMode = serializedObject.FindProperty(nameof(_cardMode));
-            _cardZMultiplier = serializedObject.FindProperty(nameof(_cardZMultiplier));
-            _manuallyHandleCardAnimations = serializedObject.FindProperty(nameof(_manuallyHandleCardAnimations));
         }
         
         public override void OnInspectorGUI()
         {
             base.OnInspectorGUI();
             EditorGUILayout.PropertyField(_swipeThreshold);
-            EditorGUILayout.PropertyField(_cardMode);
-            if (_cardMode.boolValue)
-            {
-                EditorGUILayout.Space();
-                EditorGUILayout.PropertyField(_cardZMultiplier);
-                EditorGUILayout.PropertyField(_manuallyHandleCardAnimations);
-            }
-            
             serializedObject.ApplyModifiedProperties();
         }
     }
--- Assets/Scripts/RSRPages.cs
+++ Assets/Scripts/RSRPages.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -7,9 +6,6 @@ namespace RecyclableSR
     public class RSRPages : RSR
     {
         [SerializeField] protected float _swipeThreshold = 200;
-        [SerializeField] private float _cardZMultiplier;
-        [SerializeField] private bool _cardMode;
-        [SerializeField] private bool _manuallyHandleCardAnimations;
 
         protected IPageSource _pageSource;
         private bool _isDragging;
@@ -40,52 +36,6 @@ namespace RecyclableSR
             {
                 _pageSource?.PageWillFocus(_currentPage, true, _visibleItems[_currentPage].item, _visibleItems[_currentPage].transform, _itemPositions[_currentPage].topLeftPosition);
                 _pageSource?.PageFocused(_currentPage, true, _visibleItems[_currentPage].item);
-            }
-            
-            SetCardsZIndices();
-        }
-
-        /// <summary>
-        /// Set item z index on card when after it finishes scrolling
-        /// also set item canvas group intractability & order in canvas
-        /// </summary>
-        private void SetCardsZIndices(int pageToStaggerAnimationFor = -1)
-        {
-            if (!_cardMode)
-                return;
-            
-            var childrenSiblingOrder = new SortedDictionary<int, Transform>();
-            foreach (var visibleItem in _visibleItems)
-            {
-                // set card z position
-                var itemZIndex = Mathf.Abs(visibleItem.Key -_currentPage) * _cardZMultiplier;
-                var itemPosition = visibleItem.Value.transform.anchoredPosition3D; 
-                itemPosition.y = 0;
-                itemPosition.z = itemZIndex;
-                visibleItem.Value.transform.anchoredPosition3D = itemPosition;
-
-                // set card as interactable if is current index
-                visibleItem.Value.item.CanvasGroup.interactable = (visibleItem.Key == _currentPage);
-                visibleItem.Value.item.CanvasGroup.blocksRaycasts = (visibleItem.Key == _currentPage);
-                
-                // sort items
-                var siblingOrder = visibleItem.Key - _currentPage;
-                if (siblingOrder > 0)
-                    siblingOrder = siblingOrder * -1 - _currentPage;
-
-                visibleItem.Value.item.CanvasGroup.alpha = visibleItem.Key >= _currentPage ? 1 : 0;
-                childrenSiblingOrder.Add(siblingOrder, visibleItem.Value.transform);
-            }
-
-            foreach (var child in childrenSiblingOrder)  
-            {
-                child.Value.SetAsLastSibling();
-            }
-
-            if (pageToStaggerAnimationFor != -1)
-            {
-                _visibleItems[pageToStaggerAnimationFor].item.CanvasGroup.alpha = 1;
-                _visibleItems[pageToStaggerAnimationFor].transform.SetAsLastSibling();
             }
         }
 
@@ -148,64 +98,34 @@ namespace RecyclableSR
             if (_currentPage != itemIndex)
             {
                 var isNextPage = itemIndex > _currentPage;
-                var pageToStaggerAnimation = -1;
-                if (!instant)
-                {
-                    if (_manuallyHandleCardAnimations && isNextPage)
-                        pageToStaggerAnimation = _currentPage;
-                }
-
                 if (_visibleItems.TryGetValue(_currentPage, out var visibleItem))
                     _pageSource?.PageUnFocused(_currentPage, isNextPage, visibleItem.item);
 
                 _currentPage = itemIndex;
+                
                 if (_forceCallWillFocusAfterAnimation)
                     _pageSource?.PageWillFocus(_currentPage, isNextPage, _visibleItems[_currentPage].item, _visibleItems[_currentPage].transform, _itemPositions[_currentPage].topLeftPosition);
                 _pageSource?.PageFocused(_currentPage, isNextPage, _visibleItems[_currentPage].item);
-                
-                SetCardsZIndices(pageToStaggerAnimation);
             }
         }
 
         public override void OnBeginDrag(PointerEventData eventData)
         {
-            if (!_cardMode)
-                base.OnBeginDrag(eventData);
-
+            base.OnBeginDrag(eventData);
             _isDragging = true;
             _dragStartingPosition = content.anchoredPosition * (vertical ? 1 : -1);
         }
         
-        /// <summary>
-        /// only used in cards mode, this overrides the dragging behavior of scroll view and moves the cards by themselves
-        /// </summary>
-        /// <param name="eventData"></param>
-        public override void OnDrag(PointerEventData eventData)
-        {
-            if (!_cardMode)
-                base.OnDrag(eventData);
-
-            if (!_isDragging || !_cardMode)
-                return;
-
-            var deltaMovement = eventData.delta;
-            deltaMovement[1 - _axis] = 0;
-            _visibleItems[_currentPage].transform.anchoredPosition += deltaMovement;
-        }
-
         public override void OnEndDrag(PointerEventData eventData)
         {
-            if (!_cardMode)
-                base.OnEndDrag(eventData);
+            base.OnEndDrag(eventData);
             
             if (!_isDragging)
                 return;
             
             _isDragging = false;
-            var newPage = CalculateNextPageAfterDrag();
-            // TODO: why was this here?
-            // _dataSource.ScrolledToItem(_visibleItems[newPage].item, newPage);
-            ScrollToItem(newPage);
+            var newPageIndex = CalculateNextPageAfterDrag();
+            ScrollToItem(newPageIndex);
         }
 
         protected virtual int CalculateNextPageAfterDrag()
