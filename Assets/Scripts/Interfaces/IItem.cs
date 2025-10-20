d0100f939ede8b72fbc06d8a59555064a7df7b24
Maged Farid <magedf.90@gmail.com>
2025-10-20 23:21:50 +0300

    Added license header to all files

--- Assets/Scripts/Interfaces/IItem.cs
+++ Assets/Scripts/Interfaces/IItem.cs
@@ -1,3 +1,5 @@
+// Copyright (c) 2025 Maged Farid
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
 using UnityEngine;
 
 namespace RecyclableSR
